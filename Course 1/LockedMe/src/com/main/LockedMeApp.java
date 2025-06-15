cdcb5d16c95cc3ee1b6553e3d247a9a0a2fdc23b
unknown <vedant2004024@gmail.com>
2025-06-15 20:21:59 +0530

    feat: completed main menu

--- Course 1/LockedMe/src/com/main/LockedMeApp.java
+++ Course 1/LockedMe/src/com/main/LockedMeApp.java
@@ -1,10 +1,52 @@
 package com.main;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class LockedMeApp {
 
+	private static Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		File directory = new File("LockedMeApp");
+		boolean directoryCreated = directory.mkdir();
+		
+//		if(directoryCreated)
+//			System.out.println("New directory created");
+//		else
+//			System.out.println("Directory already exists. Using existing directory");
+
+		System.out.println("App Name: LockedMe");
+		System.out.println("Developed by: Vedant Verma");
+		System.out.println("Organization: Company Lockers Pvt. Ltd. ");
+		
+		while(true) {
+			
+			int choice = scanner.nextInt();
+			
+			System.out.println("Choose one of the following options as required: ");
+			System.out.println("1. Display all files in an ascending order.");
+			System.out.println("2. Add/Search/Delete a file.");
+			System.out.println("3. Exit the application.");
+			
+			switch(choice) {
+			
+			case 1: break;
+			case 2: break;
+			case 3: System.out.println("Exited the application.");
+					return;
+			default: System.out.println("Enter a valid choice...\n");
+			
+			
+			}
+		}
+		
+		
+		
+		
+		
+		
 	}
 
 }
