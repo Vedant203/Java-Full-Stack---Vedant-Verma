b66c96be50a4efc073fb39ea46935dc4226439ef
unknown <vedant2004024@gmail.com>
2025-06-21 21:47:28 +0530

    feat: Added CRUD operations

--- Course 1/LockedMe/src/com/main/LockedMeApp.java
+++ Course 1/LockedMe/src/com/main/LockedMeApp.java
@@ -3,6 +3,7 @@ package com.main;
 import java.io.File;
 import java.util.Scanner;
 
+
 public class LockedMeApp {
 
 	private static Scanner scanner = new Scanner(System.in);
@@ -10,7 +11,7 @@ public class LockedMeApp {
 	public static void main(String[] args) {
 
 		File directory = new File("LockedMeApp");
-		boolean directoryCreated = directory.mkdir();
+		boolean directoryCreated = directory.mkdirs();
 		
 //		if(directoryCreated)
 //			System.out.println("New directory created");
@@ -23,30 +24,57 @@ public class LockedMeApp {
 		
 		while(true) {
 			
-			int choice = scanner.nextInt();
 			
 			System.out.println("Choose one of the following options as required: ");
 			System.out.println("1. Display all files in an ascending order.");
 			System.out.println("2. Add/Search/Delete a file.");
 			System.out.println("3. Exit the application.");
 			
+			int choice = scanner.nextInt();
+			
 			switch(choice) {
 			
 			case 1: break;
-			case 2: break;
+			case 2: while(true) {
+				System.out.println("CRUD operations menu:");
+				System.out.println("1. Add a file");
+				System.out.println("2. Delete a file");
+				System.out.println("3. Search a file");
+				System.out.println("4. Go back to main menu");
+				System.out.println("Enter your choice: ");
+
+				int choice1 = scanner.nextInt();
+				
+				switch(choice1) {
+					case 1: 
+						System.out.println("Enter the name of the file to add:");
+						String addFileName = scanner.next();
+						// Code to add file
+						System.out.println("File " + addFileName + " added successfully.");
+						break;
+					case 2: 
+						System.out.println("Enter the name of the file to delete:");
+						String deleteFileName = scanner.next();
+						// Code to delete file
+						System.out.println("File " + deleteFileName + " deleted successfully.");
+						break;
+					case 3: 
+						System.out.println("Enter the name of the file to search:");
+						String searchFileName = scanner.next();
+						// Code to search file
+						System.out.println("File " + searchFileName + " found.");
+						break;
+					case 4: 
+						break; // Go back to main menu
+					default: 
+						System.out.println("Invalid choice. Please try again.\n");
+				}
+			}
 			case 3: System.out.println("Exited the application.");
 					return;
 			default: System.out.println("Enter a valid choice...\n");
 			
-			
 			}
 		}
-		
-		
-		
-		
-		
-		
 	}
-
 }
